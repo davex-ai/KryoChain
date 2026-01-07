5b6456e7111bb3ff1ed218dc32b8e852e240438f
davex-ai <omotoyedave4@gmail.com>
2026-01-07 03:34:50 +0100

    init

---
+++ src/main/java/dave/blockchain/Block.java
@@ -0,0 +1,4 @@
+package dave.blockchain;
+
+public class Block {
+}
---
+++ src/main/java/dave/blockchain/Main.java
@@ -0,0 +1,7 @@
+package dave.blockchain;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
