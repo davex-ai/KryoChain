7cf6fccedfa71c0e6c706e8b28a75ad61e7b5812
davex-ai <omotoyedave4@gmail.com>
2026-01-07 13:12:54 +0100

    block chain

--- src/main/java/dave/blockchain/Block.java
+++ src/main/java/dave/blockchain/Block.java
@@ -7,7 +7,8 @@ public class Block {
     public String hash;
     public String previousHash;
     private String data;
-    private long timeStamp; 
+    private long timeStamp;
+    private int nonce;
 
     public Block(String data,String previousHash ) {
         this.data = data;
@@ -20,8 +21,18 @@ public class Block {
         String calculatedhash = StringUtil.applySha256(
                 previousHash +
                         Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
                         data
         );
         return calculatedhash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
